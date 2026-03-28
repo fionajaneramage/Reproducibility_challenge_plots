18049137c7bc342542df46fcfdd79541aed7c210
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2026-03-28 19:21:54 +0000

    update column names

--- replication.R
+++ replication.R
@@ -1,22 +1,31 @@
 # Load required R packages
-library(tidyverse)
-library(stringr)
-library(forcats)
-library(tidyr)
-library(patchwork)
+library(dplyr)
+library(readr)
 
 # Import data
 meta_analysis <- read_csv("Data/Entered/Meta-analysis.csv")
 
-replication_analysis <- df %>% 
+meta_analysis <- meta_analysis %>%
+  filter(!is.na(Outcome)) %>%
+  rename(
+    Orig_ES  = `Original effect size`,
+    Orig_LCI = `Original LCI`,
+    Orig_UCI = `Original UCI`,
+    Rep_ES   = `Replication effect size`,
+    Rep_LCI  = `Replication LCI`,
+    Rep_UCI  = `Replication UCI`,
+    Method = `Method details`
+  )
+
+replication_analysis <- meta_analysis %>% 
     mutate(
       # 1. CI widths
       width_orig = Orig_UCI - Orig_LCI,
       width_rep  = Rep_UCI - Rep_LCI,
       
       # 2. Relative differences
-      est_diff   = abs(Rep_ES - Orig_ES) / ((Rep_ES + Orig_ES)/2),
-      width_diff = abs(width_rep - width_orig) / ((width_rep = width_orig)/2),
+      est_diff   = abs(Rep_ES - Orig_ES) / abs((Rep_ES + Orig_ES)/2),
+      width_diff = abs(width_rep - width_orig) / abs((width_rep + width_orig)/2),
       
       # ---- CRITERION 1: within 10% ----
       est_within_10pct   = est_diff <= 0.10,
